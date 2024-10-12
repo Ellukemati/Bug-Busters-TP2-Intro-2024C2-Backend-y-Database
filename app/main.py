4f47466a7d91ec0af4eabb8e35370783e6817d63
Joaquin Spallanzani <jspallanzani@fi.uba.ar>
2024-10-12 20:12:12 -0300

    Agrego test a los dos endpoints

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from routers.moves import router as moves_router
-from routers.pokemons import router as pokemons_router
-from routers.teams import router as teams_router
+
+# from routers.moves import router as moves_router
+from app.routers.pokemons import router as pokemons_router
+
+# from routers.teams import router as teams_router
 
 app = FastAPI()
 
-app.include_router(moves_router, prefix="/moves", tags=["moves"])
+# app.include_router(moves_router, prefix="/moves", tags=["moves"])
 app.include_router(pokemons_router, prefix="/pokemons", tags=["pokemons"])
-app.include_router(teams_router, prefix="/teams", tags=["teams"])
+# app.include_router(teams_router, prefix="/teams", tags=["teams"])
--- test/test_pokemons.py
+++ test/test_pokemons.py
@@ -1,5 +1,49 @@
 from fastapi.testclient import TestClient
+from app.models import Pokemon
+from app.main import app
+from app.routers.pokemons import pokemons
 
-from main import app
 
 client = TestClient(app)
+
+
+def test_crear_pokemon():
+    nuevo_pokemon = {
+        "id": 10000,
+        "nombre": "Pikachu",
+        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10000.png",
+        "tipo": "Electrico",
+    }
+
+    response = client.post("/pokemons/", json=nuevo_pokemon)
+
+    assert response.status_code == 201
+    assert response.json() == nuevo_pokemon
+
+
+def test_crear_pokemon_con_id_existente():
+    pokemon_existente = {
+        "id": 10000,
+        "nombre": "Pikachu",
+        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10000.png",
+        "tipo": "Electrico",
+    }
+    pokemons.append(pokemon_existente)
+
+    response = client.post("/pokemons/", json=pokemon_existente)
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Ya existe un pokemon con ese id"
+
+
+def test_borrar_un_pokemon():
+    pokemon_existente = {
+        "id": 10000,
+        "nombre": "Pikachu",
+        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10000.png",
+        "tipo": "Electrico",
+    }
+    pokemons.append(pokemon_existente)
+    response = client.delete("pokemons/10000")
+
+    assert response.status_code == 200
