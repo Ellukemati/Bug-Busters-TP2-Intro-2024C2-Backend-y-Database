8193d694008c83c402d75ceb5804959fe13e6db4
Joaquin Spallanzani <jspallanzani@fi.uba.ar>
2024-10-14 21:31:36 -0300

    Arreglo estructura del proyecto y el modelo de pokemon

---
+++ models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class Pokemon(BaseModel):
+    pokemon_id: int
+    nombre: str
+    imagen: str
+    tipos: list[str]
+    habilidades: list[str]
+    altura: int
+    peso: int
+    estadisticas: dict[str, int]
+    tipo: str
--- test/test_pokemons.py
+++ test/test_pokemons.py
@@ -1,49 +1,39 @@
 from fastapi.testclient import TestClient
-from app.models import Pokemon
-from app.main import app
+from models import Pokemon
+from main import app
 from app.routers.pokemons import pokemons
 
 
 client = TestClient(app)
 
 
-def test_crear_pokemon():
-    nuevo_pokemon = {
-        "id": 10000,
-        "nombre": "Pikachu",
-        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10000.png",
-        "tipo": "Electrico",
-    }
-
-    response = client.post("/pokemons/", json=nuevo_pokemon)
-
-    assert response.status_code == 201
-    assert response.json() == nuevo_pokemon
-
-
-def test_crear_pokemon_con_id_existente():
-    pokemon_existente = {
-        "id": 10000,
-        "nombre": "Pikachu",
-        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10000.png",
-        "tipo": "Electrico",
-    }
-    pokemons.append(pokemon_existente)
-
-    response = client.post("/pokemons/", json=pokemon_existente)
-
-    assert response.status_code == 400
-    assert response.json()["detail"] == "Ya existe un pokemon con ese id"
-
-
 def test_borrar_un_pokemon():
     pokemon_existente = {
-        "id": 10000,
-        "nombre": "Pikachu",
-        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10000.png",
-        "tipo": "Electrico",
+        "pokemon_id": 392,
+        "nombre": "infernape",
+        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png",
+        "tipos": ["Fuego", "Lucha"],
+        "habilidades": ["Mar Llamas", "Puño Férreo"],
+        "altura": 12,
+        "peso": 550,
+        "estadisticas": {
+            "hp": 76,
+            "attack": 104,
+            "defense": 71,
+            "special-attack": 104,
+            "special-defense": 71,
+            "speed": 108,
+            "accuracy": 100,
+            "evasion": 100,
+        },
     }
     pokemons.append(pokemon_existente)
-    response = client.delete("pokemons/10000")
+    response = client.delete("pokemons/392")
 
     assert response.status_code == 200
+
+
+def test_borrar_pokemon_no_existe():
+    response = client.delete("/pokemons/10")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "No se encontro ese id en nuestros pokemons"
