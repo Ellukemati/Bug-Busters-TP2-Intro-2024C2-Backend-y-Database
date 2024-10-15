ddf6dfbef01783d233f87cf4dbbcafde9541a122
Joaquin Spallanzani <jspallanzani@fi.uba.ar>
2024-10-15 16:44:53 -0300

    Se arreglo un error con los test

--- test/test_pokemons.py
+++ test/test_pokemons.py
@@ -60,11 +60,30 @@ def test_crear_pokemon_con_id_existente():
 
     assert response.status_code == 400
     assert response.json()["detail"] == "Ya existe un pokemon con ese id"
-    
-    
-    
-    
-    #se rompio algo?
+
+
+def test_borrar_un_pokemon():
+    pokemon_existente = {
+        "pokemon_id": 392,
+        "nombre": "infernape",
+        "imagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/392.png",
+        "tipos": ["Fuego", "Lucha"],
+        "habilidades": ["Mar Llamas", "Puño Férreo"],
+        "altura": 12,
+        "peso": 550,
+        "estadisticas": {
+            "hp": 76,
+            "attack": 104,
+            "defense": 71,
+            "special-attack": 104,
+            "special-defense": 71,
+            "speed": 108,
+            "accuracy": 100,
+            "evasion": 100,
+        },
+        "cadena_evolutiva": [390, 391, 392],
+    }
+    pokemons.append(pokemon_existente)
     response = client.delete("pokemons/392")
 
     assert response.status_code == 200
@@ -74,4 +93,3 @@ def test_borrar_pokemon_no_existe():
     response = client.delete("/pokemons/10")
     assert response.status_code == 404
     assert response.json()["detail"] == "No se encontro ese id en nuestros pokemons"
-
