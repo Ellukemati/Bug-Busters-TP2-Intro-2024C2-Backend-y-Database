2e99c1183840c843854b8e7808189147e0fad9a6
MartinZaton <mzaton@fi.uba.ar>
2024-10-15 17:51:32 -0300

    Creo pruebas, corrijo los tipos en las pruebas anteriores (causaban conflictos)

--- test/test_teams.py
+++ test/test_teams.py
@@ -1,14 +1,14 @@
 from fastapi.testclient import TestClient
-from models import Pokemon, Equipo
+from models import Equipo
 from main import app
-from app.routers.teams import teams
+
 
 client = TestClient(app)
 
 
 def test_crear_teams_seis_pokemons():
-    equipo_con_6_pokemons = {
-        "id_equipo": 1,
+    equipo_con_6_pokemons: Equipo = {
+        "id_equipo": 3,
         "nombre": "Equipo Elite",
         "pokemons_de_equipo": [
             {
@@ -180,7 +180,7 @@ def test_crear_teams_seis_pokemons():
 
 
 def test_crear_team_con_siete_pokemons():
-    equipo_siete_pokemons = {
+    equipo_siete_pokemons: Equipo = {
         "id_equipo": 2,
         "nombre": "Equipo Sobrecargado",
         "pokemons_de_equipo": [
@@ -362,7 +362,7 @@ def test_crear_team_con_siete_pokemons():
 
 
 def test_crear_equipo_mismo_id():
-    equipo_mismo_id = {
+    equipo_mismo_id: Equipo = {
         "id_equipo": 1,
         "nombre": "Equipo Filete",
         "pokemons_de_equipo": [
@@ -524,7 +524,7 @@ def test_crear_equipo_mismo_id():
             },
         ],
     }
-    equipo_con_6_pokemons = {
+    equipo_con_6_pokemons: Equipo = {
         "id_equipo": 1,
         "nombre": "Equipo Elite",
         "pokemons_de_equipo": [
@@ -686,9 +686,187 @@ def test_crear_equipo_mismo_id():
             },
         ],
     }
-    teams.append(equipo_con_6_pokemons)
+    client.post("/teams", json=equipo_con_6_pokemons)
 
     response = client.post("/teams", json=equipo_mismo_id)
 
     assert response.status_code == 400
     assert response.json()["detail"] == "Ya existe un equipo con ese id"
+
+
+def test_borrar_equipo_existente():
+
+    equipo: Equipo = {
+        "id_equipo": 2,
+        "nombre": "Equipo Elite",
+        "pokemons_de_equipo": [
+            {
+                "id": 25,
+                "nombre": "Pikachu",
+                "naturaleza": {
+                    "id": 1,
+                    "nombre": "Activa",
+                    "aumenta_estadistica": "Velocidad",
+                    "reduce_estadistica": "Defensa",
+                },
+                "movimientos": [
+                    {
+                        "id": 1,
+                        "nombre": "Impactrueno",
+                        "tipo": "Eléctrico",
+                        "power": 40,
+                        "accuracy": 100,
+                        "pp": 30,
+                        "generacion": "I",
+                        "categoria": "Especial",
+                        "efecto": "Parálisis",
+                        "probabilidad_efecto": 10,
+                    },
+                    {
+                        "id": 2,
+                        "nombre": "Rayo",
+                        "tipo": "Eléctrico",
+                        "power": 90,
+                        "accuracy": 100,
+                        "pp": 15,
+                        "generacion": "I",
+                        "categoria": "Especial",
+                        "efecto": "Parálisis",
+                        "probabilidad_efecto": 10,
+                    },
+                ],
+            },
+            {
+                "id": 6,
+                "nombre": "Charizard",
+                "naturaleza": {
+                    "id": 2,
+                    "nombre": "Audaz",
+                    "aumenta_estadistica": "Ataque",
+                    "reduce_estadistica": "Defensa",
+                },
+                "movimientos": [
+                    {
+                        "id": 3,
+                        "nombre": "Llamarada",
+                        "tipo": "Fuego",
+                        "power": 110,
+                        "accuracy": 85,
+                        "pp": 5,
+                        "generacion": "I",
+                        "categoria": "Especial",
+                        "efecto": "Quemadura",
+                        "probabilidad_efecto": 30,
+                    }
+                ],
+            },
+            {
+                "id": 9,
+                "nombre": "Blastoise",
+                "naturaleza": {
+                    "id": 3,
+                    "nombre": "Modesta",
+                    "aumenta_estadistica": "Ataque Especial",
+                    "reduce_estadistica": "Ataque",
+                },
+                "movimientos": [
+                    {
+                        "id": 4,
+                        "nombre": "Hidrobomba",
+                        "tipo": "Agua",
+                        "power": 110,
+                        "accuracy": 80,
+                        "pp": 5,
+                        "generacion": "I",
+                        "categoria": "Especial",
+                        "efecto": "Ninguno",
+                        "probabilidad_efecto": None,
+                    }
+                ],
+            },
+            {
+                "id": 3,
+                "nombre": "Venusaur",
+                "naturaleza": {
+                    "id": 4,
+                    "nombre": "Serena",
+                    "aumenta_estadistica": "Defensa Especial",
+                    "reduce_estadistica": "Ataque",
+                },
+                "movimientos": [
+                    {
+                        "id": 5,
+                        "nombre": "Solar Rayo",
+                        "tipo": "Planta",
+                        "power": 120,
+                        "accuracy": 100,
+                        "pp": 10,
+                        "generacion": "I",
+                        "categoria": "Especial",
+                        "efecto": "Ninguno",
+                        "probabilidad_efecto": None,
+                    }
+                ],
+            },
+            {
+                "id": 94,
+                "nombre": "Gengar",
+                "naturaleza": {
+                    "id": 5,
+                    "nombre": "Miedosa",
+                    "aumenta_estadistica": "Velocidad",
+                    "reduce_estadistica": "Defensa",
+                },
+                "movimientos": [
+                    {
+                        "id": 6,
+                        "nombre": "Bola Sombra",
+                        "tipo": "Fantasma",
+                        "power": 80,
+                        "accuracy": 100,
+                        "pp": 15,
+                        "generacion": "II",
+                        "categoria": "Especial",
+                        "efecto": "Ninguno",
+                        "probabilidad_efecto": None,
+                    }
+                ],
+            },
+            {
+                "id": 149,
+                "nombre": "Dragonite",
+                "naturaleza": {
+                    "id": 6,
+                    "nombre": "Firme",
+                    "aumenta_estadistica": "Ataque",
+                    "reduce_estadistica": "Defensa Especial",
+                },
+                "movimientos": [
+                    {
+                        "id": 7,
+                        "nombre": "Puño Fuego",
+                        "tipo": "Fuego",
+                        "power": 100,
+                        "accuracy": 100,
+                        "pp": 15,
+                        "generacion": "II",
+                        "categoria": "Físico",
+                        "efecto": "Ninguno",
+                        "probabilidad_efecto": None,
+                    }
+                ],
+            },
+        ],
+    }
+    client.post("/teams", json=equipo)
+    response = client.delete("teams/2")
+
+    assert response.status_code == 200
+    assert response.json() == equipo
+
+
+def test_borrar_equipo_no_existe():
+
+    response = client.delete("/teams/99")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "No se encontro un equipo con ese id"
