6016180b77e5b2cc42ea9dd82fad77c5fcc0e1e9
Joaquin Spallanzani <jspallanzani@fi.uba.ar>
2024-10-20 20:50:39 -0300

    Elimino el router innecesario e implemento el endpoint GET/NATURES en el router teams

--- test/test_teams.py
+++ test/test_teams.py
@@ -3,14 +3,41 @@ from test.jsons import (
     equipo_mismo_id,
     equipo_siete_pokemons,
     equipo,
+    nature_1,
+    nature_2,
+    nature_3,
 )
 from fastapi.testclient import TestClient
 from app.routers.teams import teams
 from models import Equipo
 from main import app
+from app.routers.teams import Naturalezas
 
 
 client = TestClient(app)
+
+
+def test_get_natures():
+    Naturalezas.clear()
+    Naturalezas.append(nature_1)
+    Naturalezas.append(nature_2)
+    Naturalezas.append(nature_3)
+    response = client.get("teams/natures")
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data[0]["nombre"] == "hardy"
+    assert data[0]["aumenta_estadistica"] == "attack"
+    assert data[0]["reduce_estadistica"] == "attack"
+
+    assert data[1]["nombre"] == "bold"
+    assert data[1]["aumenta_estadistica"] == "defense"
+    assert data[1]["reduce_estadistica"] == "attack"
+
+    assert data[2]["nombre"] == "modest"
+    assert data[2]["aumenta_estadistica"] == "special-attack"
+    assert data[2]["reduce_estadistica"] == "attack"
 
 
 def test_crear_teams_seis_pokemons():
