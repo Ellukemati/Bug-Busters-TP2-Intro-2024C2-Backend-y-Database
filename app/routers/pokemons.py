7bf51c1be901d07c247b2ba7725fdabeef193a02
Joaquin Spallanzani <jspallanzani@fi.uba.ar>
2024-10-20 17:58:40 -0300

    .

--- app/routers/pokemons.py
+++ app/routers/pokemons.py
@@ -160,34 +160,6 @@ def crear_pokemon(pokemon: Pokemon) -> Pokemon:
     return pokemon
 
 
-Naturalezas: list[Naturaleza] = []
-estadisticas = {}
-
-with open("stats.csv", mode="r") as estadisticas_file:
-    reader = csv.DictReader(estadisticas_file)
-    for row in reader:
-        estadisticas[row["id"]] = row["identifier"]
-with open("natures.csv", mode="r") as naturalezas_file:
-    reader = csv.DictReader(naturalezas_file)
-    for row in reader:
-        id_aumenta = row["increased_stat_id"]
-        id_disminuye = row["decreased_stat_id"]
-        nombre_aumenta = estadisticas.get(id_aumenta)
-        nombre_disminuye = estadisticas.get(id_disminuye)
-        naturaleza = Naturaleza(
-            id=int(row["id"]),
-            nombre=row["identifier"],
-            aumenta_estadistica=nombre_aumenta,
-            reduce_estadistica=nombre_disminuye,
-        )
-        Naturalezas.append(naturaleza)
-
-
-@router.get("/natures")
-def show_natures() -> list[Naturaleza]:
-    return Naturalezas
-
-
 @router.delete("/{id}")
 def borrar_pokemon(id: int):
     for a in POKEMON_DATA:
