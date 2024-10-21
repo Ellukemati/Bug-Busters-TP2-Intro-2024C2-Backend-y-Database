50767be4313748d45fcaa2f06bf4d669e269b377
Asimbron <asimbron@fi.uba.ar>
2024-10-21 22:43:29 +0100

    cambios en test_pokemons.py: se sacaron algunas lineas vacias

--- test/test_pokemons.py
+++ test/test_pokemons.py
@@ -3,7 +3,6 @@ from app.routers.pokemons import POKEMON_DATA
 from test.jsons import infernape_mock, movimientos_infernape
 from models import Pokemon, Naturaleza
 from main import app
-
 from jsons import nature_1, nature_2, nature_3, infernape_mock, movimientos_infernape
 import pytest
 
@@ -18,35 +17,29 @@ def test_get_pokemons():
     
 def test_get_pokemon_encontrado(client):
     response = client.get("/pokemons/392")
-
     assert response.status_code == 200
     assert response.json() == infernape_mock
 
 
 def test_get_pokemon_no_encontrado(client):
     response = client.get("/pokemons/0")  # ID 0 no existe.
-
     assert response.status_code == 404
     assert response.json() == {"detail": "Pokémon no encontrado."}
 
 def test_get_pokemon_movimientos_encontrado():
     response = client.get("/pokemons/392/moves")
-
     assert response.status_code == 200
     assert response.json() == movimientos_infernape
 
 def test_get_pokemon_movimientos_no_encontrado():
     response = client.get("/pokemons/0/moves")  # ID 0 no existe
-
     assert response.status_code == 404
     assert response.json() == {"detail": "Pokémon no encontrado."}
 
 def test_borrar_un_pokemon():
     POKEMON_DATA.clear()
-
     POKEMON_DATA.append(infernape_mock)
     response = client.delete("pokemons/392")
-
     assert response.status_code == 200
 
 
@@ -59,9 +52,7 @@ def test_borrar_pokemon_no_existe():
 
 def test_crear_pokemon():
     POKEMON_DATA.clear()
-
     response = client.post("/pokemons", json=infernape_mock)
-
     assert response.status_code == 201
     assert response.json() == infernape_mock
 
