2f20711bf2b991b43e646d8191f825e134538e43
Asimbron <imac@iMac.fibertel.com.ar>
2024-10-30 16:21:25 -0300

    se empiezan a desarrollar los seeds

--- app/db/database.py
+++ app/db/database.py
@@ -1,16 +1,35 @@
 from typing import Generator, Annotated
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import SQLModel, Session, create_engine, select
 from fastapi import Depends
+from main import logger
+
+from app.models.naturaleza import Naturaleza
+from app.db.cargar_naturalezas import cargar_naturalezas
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('_main_.')
 
 SQLITE_FILE_PATH = "app/db/database.db"
 
 engine = create_engine(f"sqlite:///{SQLITE_FILE_PATH}")
 
+
 def get_db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
 
+
 SessionDep = Annotated[Session, Depends(get_db)]
+
 
 def init_db():
     SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+
+        if not session.exec(select(Naturaleza)).first():
+            logger.info("Cargando naturalezas...")
+            cargar_naturalezas(session)
+            logger.info("Naturalezas cargadas con exito.")
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from app.main import api_router
+import logging
+from app.db.database import init_db
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(_name_)
 
 app = FastAPI()
 
 app.include_router(api_router)
+
+
+@app.on_event("startup")
+def startup_event():
+    logger.info("Inicializando la base de datos y cargando datos si es necesario...")
+    init_db()
+    logger.info("Inicializacion completa.")
