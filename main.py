559a0286a5180054a3063af00849fca075ad29c6
MartinZaton <mzaton@fi.uba.ar>
2024-11-10 12:42:08 -0300

    Aplico cambios sugeridos: se inicializa la db sin recibir warnings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.main import api_router
 import logging
 from app.db.database import init_db
+from contextlib import asynccontextmanager
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,8 +12,14 @@ app = FastAPI()
 app.include_router(api_router)
 
 
-@app.on_event("startup")
-def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     logger.info("Inicializando la base de datos y cargando datos si es necesario...")
     init_db()
     logger.info("Inicialización completa.")
+    yield
+
+    logger.info("Aplicación cerrándose.")
+
+
+app.router.lifespan_context = lifespan
