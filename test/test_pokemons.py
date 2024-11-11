9d192763de83cfd5597c0019d9c50d460983a18a
Joaquin Spallanzani <jspallanzani@fi.uba.ar>
2024-11-11 03:30:51 -0300

    Arreglo un restante de imports en test_pokemons

--- test/test_pokemons.py
+++ test/test_pokemons.py
@@ -1,13 +1,6 @@
 from fastapi.testclient import TestClient
-from app.routers.pokemons import POKEMON_DATA
 from app.models.pokemon import Pokemon
 from main import app
-from jsons import (
-    infernape_mock,
-    movimientos_infernape,
-    infernape_mock_post,
-    infernape_mock_id1,
-)
 import pytest
 from sqlmodel import Session
 from app.db.database import SessionDep
@@ -16,13 +9,13 @@ from app.models.movimiento import Movimiento
 from app.models.pokemonMovimiento import PokemonMovimiento
 
 from test.jsons import (
-    infernape_mock, 
-    movimientos_infernape, 
+    infernape_mock,
+    movimientos_infernape,
     relaciones_infernape,
     infernape_mock_post,
     infernape_mock_id1,
-    infernape_json
-    )
+    infernape_json,
+)
 
 client = TestClient(app)
 
@@ -39,12 +32,11 @@ def test_get_pokemon_no_encontrado(session: Session, client: TestClient):
     assert response.status_code == 404
 
 
-
 def test_get_pokemon_movimientos_encontrado(session: Session, client: TestClient):
     pokemon = Pokemon(**infernape_json)
     movimientos = [Movimiento(**data) for data in movimientos_infernape]
     pokemon_movimientos = [PokemonMovimiento(**data) for data in relaciones_infernape]
-    
+
     session.add(pokemon)
     session.add_all(movimientos)
     session.add_all(pokemon_movimientos)
@@ -52,14 +44,15 @@ def test_get_pokemon_movimientos_encontrado(session: Session, client: TestClient
     response = client.get("/pokemons/392/moves")
     content = response.json()
     assert response.status_code == 200
-    assert sorted(content, key=lambda x: x['id']) == sorted(movimientos_infernape, key=lambda x: x['id']) #ordena las listas, para poder ser comparadas
+    assert sorted(content, key=lambda x: x["id"]) == sorted(
+        movimientos_infernape, key=lambda x: x["id"]
+    )  # ordena las listas, para poder ser comparadas
 
 
 def test_get_pokemon_movimientos_no_encontrado(session: Session, client: TestClient):
     response = client.get("/pokemons/0/moves")  # ID 0 no existe
     assert response.status_code == 404
     assert response.json() == {"detail": "Pokémon no encontrado."}
-
 
 
 def test_borrar_un_pokemon(session: Session, client: TestClient):
